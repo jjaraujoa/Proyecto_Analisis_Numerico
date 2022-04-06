45de7d00de6a0d91f0ebc4d5101937054c04731a
japintof <68928353+japintof@users.noreply.github.com>
2022-04-06 12:46:07 -0500

    Update Prueba.m

--- Prueba.m
+++ Prueba.m
@@ -16,8 +16,13 @@ Nmax= 50;
 [tabla,x,iter,errabs,errrel]=C2_biseccion(f,a,b,tol,Nmax)
 
 %% Regla Falsa
+f =@(x) x^3 - 7.51*x^2 + 18.4239*x - 14.8331;
+a=3;
+b=3.5;
+tol=0.00005;
+Nmax= 50;
 
-
+[tabla,x,iter,errabs,errrel]=C3_reglafalsa(f,a,b,tol,Nmax)
 
 %% Punto Fijo
 
